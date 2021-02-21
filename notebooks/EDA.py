2dfab79c34e64e3c1226db517a776c02a6be5e66
Navjot Bians <Navjotbians1988@gmail.com>
2021-02-20 22:58:51 -0500

    Data Pre-processing added

--- notebooks/EDA.py
+++ notebooks/EDA.py
@@ -231,71 +231,42 @@ pd.crosstab(df.toxic,df.identity_hate,margins=True).style.background_gradient(cm
 # In[79]:
 
 
-input_str = df.comment_text[0]
-input_str
+
 
 
 # In[80]:
 
 
-## Convert in lower case
-input_str = input_str.lower()
-print(input_str)
+
 
 
 # In[81]:
 
 
-### Remove numbers
-result = re.sub(r'\d+', '', input_str)
-print(result)
+
 
 
 # In[83]:
 
 
-### Remove punctuation
-result = input_str.translate(str.maketrans('','', string.punctuation))
-print(result)
+
 
 
 # In[22]:
 
 
-### Remove white spaces (removes tabs)
-input_str = input_str.strip()
-input_str
+
 
 
 # In[131]:
 
 
-### data cleaner function
-def clean(input_str):
-    input_str = input_str.lower()
-    input_str = re.sub(r'\d+', '', input_str)
-    input_str = re.sub(r"n't", " not ", input_str)
-    input_str = re.sub(r"can't", "cannot ", input_str)
-    input_str = re.sub(r"what's", "what is ", input_str)
-    input_str = re.sub(r"\'s", " ", input_str)
-    input_str = re.sub(r"\'ve", " have ", input_str)
-    input_str = re.sub(r"\'re", " are ", input_str)
-    input_str = re.sub(r"\'d", " would ", input_str)
-    input_str = re.sub(r"\'ll", " will ", input_str)
-    input_str = re.sub(r"\'scuse", " excuse ", input_str)
-    input_str = re.sub(r"I'm", "I am", input_str)
-    input_str = re.sub(r" m ", " am ", input_str)
-    input_str = re.sub('\s+', ' ', input_str)
-    input_str = re.sub('\W', ' ', input_str)
-    input_str = input_str.translate(str.maketrans('','', string.punctuation))
-    input_str = input_str.strip()
-    return input_str
+
     
 
 
 # In[132]:
 
 
-st = df.comment_text[1]
-clean(st)
+
 
