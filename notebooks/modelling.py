8cf60a6ee64889cdd58ad2f0a2bf2e4b205fc02c
Navjot Bians <Navjotbians1988@gmail.com>
2021-03-08 00:30:48 -0500

    BOW and TF-IDF description added

--- notebooks/modelling.py
+++ notebooks/modelling.py
@@ -5,7 +5,7 @@
 
 # #### Import all the required packages
 
-# In[39]:
+# In[2]:
 
 
 import pandas as pd
@@ -14,30 +14,33 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import feature_extraction,model_selection,preprocessing, naive_bayes,pipeline, manifold
 from sklearn.metrics import accuracy_score, classification_report
+import sys  
+sys.path.append('F:/AI/Toxic-comment-classifier/src')
+from word_embeddings import w_embeddings
 
 
 # #### load processed dataset
 
-# In[54]:
+# In[3]:
 
 
 df = pd.read_csv('../data/processed/processed_stem_data.csv')
 
 
-# In[55]:
+# In[4]:
 
 
 df.head()
 
 
-# In[56]:
+# In[5]:
 
 
 ### fill NA for any missing data 
 df['comment_text'].fillna("missing", inplace=True)
 
 
-# In[57]:
+# In[6]:
 
 
 labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
@@ -46,19 +49,19 @@ corpus = df['comment_text']
 
 # ### Split the date into train test datasets
 
-# In[58]:
+# In[18]:
 
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(corpus,df[labels],test_size=0.25,random_state=42)
 
 
-# In[59]:
+# In[19]:
 
 
 X_train.shape, X_test.shape
 
 
-# In[60]:
+# In[20]:
 
 
 # Stats of X_train labels
@@ -69,7 +72,7 @@ df_stats = pd.DataFrame(counts, columns=['Labels', 'number_of_comments'])
 df_stats
 
 
-# In[61]:
+# In[21]:
 
 
 #stats of X_test labels
@@ -80,27 +83,33 @@ df_stats = pd.DataFrame(counts, columns=['Labels', 'number_of_comments'])
 df_stats
 
 
-# #### Converting text comments into vectors using bag of words or TF-IDF 
+# ### Converting text comments into vectors using bag of words or TF-IDF 
 
-# In[62]:
+# We know that machine learning models doesn't accept input in the text format. So we need to convert the text data into Vector form, it is also called **Word Embeddings**. Word Embeddings can be broadly classified as:
+# 1. Frequency based - Most popular techniques are **Bag-of-Words**, **TF-IDF**
+# 2. Pridiction based - Most popular techniques are **Word2vec** and **Glove**
+# 
+# 
+
+# Here we will be using **Bag-of-Words** and **TF-IDF**<br>
+# <br>**Bag-of-Words(BOW)** - To get the embeddings from BOW we will firstly make a dictionary of words is from the test data along with the count of each word occurance in the data, then these words from the dictionary are sorted in descending order of their occurance,put these words into the columns and used as an independent features and here rows are the sentences or samples. These features will have values 0 or 1 based on if the word exists in the sentence.
+# <br>
+# **Disadvantage** of BOW - Word Embedding we get from BOW have either 0's and 1's as a values, no weights are given to the words according to their importance in the sentence. That means we can not get the sementics of the sentence.
+
+# **TF-IDF** - It stands for Term Frequency - Inverse Document Frequency
+# <br> To get embedding with TF-IDF, we calculate Term frequency and Inverse Document Frequency seperate and then multiply them together to get TF-IDF.
+# Formulas to calculate TF-IDF: <br>
+# **TF** : $$\frac{Number\, of\, repetition\, of\, word\, in\, a\, sentence}{Number\, of\, words\, in\, a\, sentence}$$ 
+# **IDF**:$$log\Bigg[\frac{Total\, Number\, of\,sentences}{Number\, of\, sentences\, containing\, the \, word}\Bigg]$$ 
+# <br>
+# **TF-IDF**: $$\Bigg[\frac{Number\, of\, repetition\, of\, word\, in\, a\, sentence}{Number\, of\, words\, in\, a\, sentence}\Bigg]*log\Bigg[\frac{Total\, Number\, of\,sentences}{Number\, of\, sentences\, containing\, the \, word}\Bigg]$$ 
+# <br>In **TF-IDF** also, we need dictionary of words with their count of occurance to do the calculation. **TF** assign more weightage to the word which repeat multiple times in the sentance where as **IDF** decreases the weightage to word as number of sentences containing the increases. Here, feature vectors not only contains 0's and 1' but does contain other other values depending on the word importance in the sentence. This is retaining the sementics of the sentence to some extent so it should perform better than BOW.
+# <br>Here **TF-IDG** can have zero value for the word which existed in every sentence and give more weightage to less often occured words that means it could cause over-fitting problem but that is yet to discove. 
+
+# In[11]:
 
 
-def word_embeddings(X_train, X_test, embedding_type = "tfidf"):
-    if embedding_type == "bow":
-        bw_vectorizer = feature_extraction.text.CountVectorizer(max_features= 100)
-        X_train = bw_vectorizer.fit_transform(X_train).toarray()
-        X_test = bw_vectorizer.fit_transform(X_test).toarray()
-    if embedding_type == "tfidf":
-        tf_vectorizer = feature_extraction.text.TfidfVectorizer(max_features=100)
-        X_train = tf_vectorizer.fit_transform(X_train).toarray()
-        X_test = tf_vectorizer.fit_transform(X_test).toarray()
-    return X_train, X_test
-
-
-# In[63]:
-
-
-Xv_train, Xv_test = word_embeddings(X_train, X_test, "tfidf")
+Xv_train, Xv_test = w_embeddings(X_train, X_test, "tfidf")
 
 
 # ### Training
@@ -120,10 +129,9 @@ for label in labels:
 
 
 # <br> Checked the impact of use of Bag of words and TF-IDF on the accuracy of Linear regression. 
-# <br>In this use case accuracy:
-# <br>Remained same - Identity hate, threat
-# <br>Almost same - Severe_toxic
-# <br>Little bit improved with the use of TF-IDF but not very significant change - Toxic, Obscene, insult
+# <br>Accuracy remained same - `Identity hate`, `threat`
+# <br>Accuracy remained same - `Severe_toxic`
+# <br>Accuracy improved little bit  with the use of TF-IDF but not very significant change for `Toxic`, `Obscene`, `insult`
 
 # In[64]:
 
