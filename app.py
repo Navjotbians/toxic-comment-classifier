f5dd371d5c8d615388c81dd9e94765d09e2a7f59
Navjot Bians <Navjotbians1988@gmail.com>
2021-03-19 13:00:14 -0400

    Update condition based on used input

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ dir_path = os.getcwd()
 
 app = Flask(__name__)
 
+### load model
 clf_file = os.path.join(dir_path, 'model', 'final_model.pkl')
 clf = pickle.load(open(clf_file, 'rb'))
 
-# 'model/final_vectorizer.pkl'
-# 'model/final_model.pkl'
 
 ### load vectorizer
 vec_file = os.path.join(dir_path, 'model', 'final_vectorizer.pkl')
@@ -18,33 +17,31 @@ vectorizer = pickle.load(open(vec_file, 'rb'))
 
 @app.route('/')
 
-def my_form():
+def home():
 	return render_template('form1.html', p = "")
 
-# @app.route('/', methods=['POST'])
-# def my_form_post():
-# 	text = request.form['u']
-# 	processed_text = text.upper()
-# 	return processed_text
 
 @app.route('/', methods = ['POST'])
 def check_toxicity():
-	text = request.form['u']
-	processed_text = text.lower()
-	processed_text = vectorizer.transform([processed_text])
-	prediction = clf.predict(processed_text)
-
-
-	labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-	predc = []
-	i = [labels[i] for i in range (0,len(prediction[0])) if prediction[0][i] == 1]
-	if len(i)== 0:
-		out ='Comment is not toxic'
-		return render_template('form1.html', p = out)
+	if len(request.form['u']) == 0:
+		return render_template('form1.html', p = "")
 	else:
-		out = " | ".join(i)
-		return render_template('form1.html', p = out)
-	return render_template('form1.html', p = "")
+		text = request.form['u']
+		processed_text = text.lower()
+		processed_text = vectorizer.transform([processed_text])
+		prediction = clf.predict(processed_text)
+
+
+		labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+		predc = []
+		i = [labels[i] for i in range (0,len(prediction[0])) if prediction[0][i] == 1]
+		if len(i)== 0:
+			out ='Comment is not toxic'
+			return render_template('form1.html', p = out)
+		else:
+			out = " | ".join(i)
+			return render_template('form1.html', p = out)
+	
 
 	
 
